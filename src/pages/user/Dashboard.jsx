3cbb18435149954a0ad7c1fc572fa5bb2056947e
sourob2356 <sourob2356@gmail.com>
2025-11-03 19:25:33 +0600

    Dashboard component for improved layout and styling

--- src/pages/user/Dashboard.jsx
+++ src/pages/user/Dashboard.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
-        <h1>Hello Karim</h1>
+    <div className='px-10 py-5'>
+        <h1 className='font-bold text-4xl text-center'>Hello <span className='text-yellow-500'>Karim</span></h1>
         
-        <div>
-          <h2>My projects</h2>
-          <div>
-              <div>
-                <h3>E-commerce</h3>
-                <button></button>
+        <div className='mt-10'>
+          <h2 className='font-bold text-xl'>My projects</h2>
+          <div className='mt-3 flex gap-5'>
+              <div className='border border-neutral-300 rounded-xl 
+              p-3 flex flex-col gap-2'>
+                <h3 className='text-lg font-bold'>E-commerce</h3>
+                <button className='bg-yellow-500 
+                px-3 py-1 rounded-lg cursor-pointer'>
+                  See 
+                </button>
               </div>
           </div>
         </div>
