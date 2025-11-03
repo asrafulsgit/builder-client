206a9c516c2550b555d73f33a7cc71654a95befe
sourob2356 <sourob2356@gmail.com>
2025-11-03 19:35:44 +0600

    update dashboard component to integrate api call

--- src/pages/user/Dashboard.jsx
+++ src/pages/user/Dashboard.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from '../../controllers/context/AuthContext'
+import { apiRequiest } from '../../utils/baseApi';
+import { toast } from 'react-toastify';
 
 const Dashboard = () => {
+  const {user} = useContext(Context);
+  const [projects,setProjects]=useState([]);
+  const getMyProjects =async()=>{
+    try {
+      const {data} = await apiRequiest('GET','/template/my');
+      setProjects(data.templates || []);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+
+  useEffect(()=>{
+    getMyProjects();
+  },[]);
+
   return (
     <div className='px-10 py-5'>
-        <h1 className='font-bold text-4xl text-center'>Hello <span className='text-yellow-500'>Karim</span></h1>
+        <h1 className='font-bold text-4xl text-center'>Hello <span className='text-yellow-500'>{user?.name}</span></h1>
         
         <div className='mt-10'>
           <h2 className='font-bold text-xl'>My projects</h2>
           <div className='mt-3 flex gap-5'>
+             {projects.map((project)=>(
               <div className='border border-neutral-300 rounded-xl 
-              p-3 flex flex-col gap-2'>
-                <h3 className='text-lg font-bold'>E-commerce</h3>
+              p-3 flex flex-col gap-2' key={project?._id}>
+                <h3 className='text-lg font-bold'>{project?.name}</h3>
                 <button className='bg-yellow-500 
                 px-3 py-1 rounded-lg cursor-pointer'>
                   See 
                 </button>
               </div>
+             )) }
           </div>
         </div>
     </div>
